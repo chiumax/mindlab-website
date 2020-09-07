d35cfb0205eafdcaa178f1c0467a15b2cc5518f2
Zaltana <dale@zaltana.io>
2020-09-07 10:16:57 -0600

    Add Project table of contents and demo projects

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,40 +8,42 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   });
 };
 
-// TODO: Add Markdown pages for summary
-
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
-// exports.createPages = async ({ graphql, actions, reporter }) => {
-//   const { createPage } = actions;
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+  const projectTemplate = path.resolve(`src/templates/project.js`);
 
-// Query for markdown nodes to use in creating pages.
-// const result = await graphql(
-//   `
-//     {
-//       allSitePage {
-//         edges {
-//           node {
-//             component
-//             path
-//           }
-//         }
-//       }
-//     }
-//   `
-// );
+  // Query for markdown nodes to use in creating pages.
+  const result = await graphql(
+    `
+      {
+        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
+            }
+          }
+        }
+      }
+    `
+  );
 
-// eslint-disable-next-line no-console
-// console.log('result', result.data.allSitePage);
+  // eslint-disable-next-line no-console
+  console.log('result', result.data.allMarkdownRemark);
 
-// Handle errors
-// if (result.errors) {
-//   reporter.panicOnBuild(`Error while running GraphQL query.`);
-// }
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+  }
 
-// result.data.sitePage.edges.forEach(edge => {
-//   createPage({
-//     path: path.resolve(edge.node.path),
-//   });
-// });
-// };
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: projectTemplate,
+      context: {}, // additional data can be passed via context
+    });
+  });
+};
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
+import { graphql, Link } from 'gatsby';
 
 import { ThemeContext } from 'providers/ThemeProvider';
 import { Container, Layout, SEO } from 'components/common';
 import { Header } from 'components/theme';
-import { PageWrapper, Details } from './styles';
+import { PageWrapper, Details, ProjectCard, ProjectCardContainer } from 'pages/styles';
 
-const Projects = () => {
+const Projects = ({ data }) => {
   const { theme } = useContext(ThemeContext);
+  const { edges: posts } = data.allMarkdownRemark;
 
   return (
     <Layout>
@@ -15,10 +17,42 @@ const Projects = () => {
       <PageWrapper as={Container}>
         <Details theme={theme}>
           <h1>Projects</h1>
+
+          <ProjectCardContainer>
+            {posts
+              .filter(post => post.node.frontmatter.title.length > 0)
+              .map(({ node: post }) => (
+                <ProjectCard key={post.id}>
+                  <h1>
+                    <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                  </h1>
+                  <h2>{post.frontmatter.date}</h2>
+                  <p>{post.excerpt}</p>
+                </ProjectCard>
+              ))}
+          </ProjectCardContainer>
         </Details>
       </PageWrapper>
     </Layout>
   );
 };
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Projects;
