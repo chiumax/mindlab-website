d9c060dd176f83544f54fb2d6f766db267229c18
dumblole <dumblole@gmail.com>
2020-10-22 18:20:51 -0400

    tag filtering for projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import tw, { css } from 'twin.macro';
 import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
-
+import Select from 'react-select';
 import { ThemeContext } from 'providers/ThemeProvider';
 import { Container, Layout, SEO } from 'components/common';
 import { Header } from 'components/theme';
@@ -17,9 +18,14 @@ import {
 } from 'styles/common-custom-styles';
 
 const Projects = ({ data }) => {
+  const [currentTag, setTag] = useState(null);
   const { theme, themeData } = useContext(ThemeContext);
   const { edges: posts } = data.allMarkdownRemark;
-
+  const tags = [...new Set(posts.map(({node:post})=> (post.frontmatter.tags)).flat())].filter((el)=>{return el!=null;});
+  const options = tags.map((tag)=>({value:tag,label:tag}))
+  console.log(options);
+  const breakpoints = useBreakpoint();
+  let select = "bruh";
   // let heroImg = post.frontmatter.hero.childImageSharp.fluid;
 
   return (
@@ -29,8 +35,20 @@ const Projects = ({ data }) => {
       <PageWrapper as={Container}>
         <Details theme={(theme, themeData)}>
           <h1>Projects</h1>
+          <Select options={options} onChange={(tag)=>setTag(tag.value)} value={currentTag} />
+          
           <ResponsiveDesktopContainer>
-            {posts.map(({ node: post }) => (
+            {(posts.filter(({node:post})=>{
+              if(currentTag == null) {
+                return true;
+              } else {
+                if(post.frontmatter.tags){
+                  console.log("bruh")
+                  console.log(currentTag);
+                  console.log(post.frontmatter.tags)
+                }
+              }
+              return currentTag==null?true:!!post.frontmatter.tags?post.frontmatter.tags.includes(currentTag):false}).map(({ node: post }) => (
               <ProjectCard key={post.id} theme={(theme, themeData)}>
                 <Link to={post.frontmatter.path}>
                   <Img fluid={post.frontmatter.hero.childImageSharp.fluid} />
@@ -53,7 +71,7 @@ const Projects = ({ data }) => {
                   </Link>
                 </AlignRight>
               </ProjectCard>
-            ))}
+            )))}
           </ResponsiveDesktopContainer>
         </Details>
       </PageWrapper>
